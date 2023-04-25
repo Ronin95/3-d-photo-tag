66f1497e12a48fffa641c231418bcfd0d301a72d
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-04-25 23:28:14 +0200

    creating a phototag app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'd3-phototag-app',
+    appId: '1:390276842825:web:81913426faddfa11c28698',
+    storageBucket: 'd3-phototag-app.appspot.com',
+    apiKey: 'AIzaSyCuJ-qRqVEWUREOMZo6J0ul8VZuQxBcgfg',
+    authDomain: 'd3-phototag-app.firebaseapp.com',
+    messagingSenderId: '390276842825',
+  },
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCuJ-qRqVEWUREOMZo6J0ul8VZuQxBcgfg",
+    authDomain: "d3-phototag-app.firebaseapp.com",
+    projectId: "d3-phototag-app",
+    storageBucket: "d3-phototag-app.appspot.com",
+    messagingSenderId: "390276842825",
+    appId: "1:390276842825:web:81913426faddfa11c28698"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'd3-phototag-app',
+    appId: '1:390276842825:web:81913426faddfa11c28698',
+    storageBucket: 'd3-phototag-app.appspot.com',
+    apiKey: 'AIzaSyCuJ-qRqVEWUREOMZo6J0ul8VZuQxBcgfg',
+    authDomain: 'd3-phototag-app.firebaseapp.com',
+    messagingSenderId: '390276842825',
+  },
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCuJ-qRqVEWUREOMZo6J0ul8VZuQxBcgfg",
+    authDomain: "d3-phototag-app.firebaseapp.com",
+    projectId: "d3-phototag-app",
+    storageBucket: "d3-phototag-app.appspot.com",
+    messagingSenderId: "390276842825",
+    appId: "1:390276842825:web:81913426faddfa11c28698"
+  }
 };
 
 /*
